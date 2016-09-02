b53e56b54e3eb497055fc3072d7084ed16bb78a4
Matthew Scott <matt@11craft.com>
2016-09-02 10:33:33 -0500

    Initial commit

---
+++ linux_android/library/src/nightradio/sunvoxlib/SunVoxLib.java
@@ -0,0 +1,78 @@
+package nightradio.sunvoxlib;
+
+import android.R.string;
+
+public class SunVoxLib
+{
+	static 
+	{
+        System.loadLibrary( "sunvox" );
+    }
+	
+	public static final int SV_INIT_FLAG_NO_DEBUG_OUTPUT = 1 << 0;
+	public static final int SV_INIT_FLAG_USER_AUDIO_CALLBACK = 1 << 1;
+	public static final int SV_INIT_FLAG_AUDIO_INT16 = 1 << 2;
+	public static final int SV_INIT_FLAG_AUDIO_FLOAT32 = 1 << 3;
+	public static final int SV_INIT_FLAG_ONE_THREAD = 1 << 4;
+
+	public static final int SV_MODULE_FLAG_EXISTS = 1;
+	public static final int SV_MODULE_FLAG_EFFECT = 2;
+	public static final int SV_MODULE_INPUTS_OFF = 16;
+	public static final int SV_MODULE_INPUTS_MASK = 255 << SV_MODULE_INPUTS_OFF;
+	public static final int SV_MODULE_OUTPUTS_OFF = 16 + 8;
+	public static final int SV_MODULE_OUTPUTS_MASK = 255 << SV_MODULE_OUTPUTS_OFF;
+
+	public static final int SV_STYPE_INT16 = 0;
+	public static final int SV_STYPE_INT32 = 1;
+	public static final int SV_STYPE_FLOAT32 = 2;
+	public static final int SV_STYPE_FLOAT64 = 3;
+	
+	public static native int audio_callback( byte[] buf, int frames, int latency, int out_time );
+	public static native int open_slot( int slot );
+	public static native int close_slot( int slot );
+	public static native int lock_slot( int slot );
+	public static native int unlock_slot( int slot );
+	public static native int init( int freq, int channels, int flags );
+	public static native int deinit();
+	public static native int get_sample_type();
+	public static native int load( int slot, string name );
+	public static native int load_from_memory( int slot, byte[] data );
+	public static native int play( int slot );
+	public static native int play_from_beginning( int slot );
+	public static native int stop( int slot );
+	public static native int set_autostop( int slot, int autostop );
+	public static native int end_of_song( int slot );
+	public static native int rewind( int slot, int line_num );
+	public static native int volume( int slot, int vol );
+	public static native int send_event( int slot, int track_num, int note, int vel, int module, int ctl, int ctl_val );
+	public static native int get_current_line( int slot );
+	public static native int get_current_line2( int slot );
+	public static native int get_current_signal_level( int slot, int channel );
+	public static native string get_song_name( int slot );
+	public static native int get_song_bpm( int slot );
+	public static native int get_song_tpl( int slot );
+	public static native int get_song_length_frames( int slot );
+	public static native int get_song_length_lines( int slot );
+	public static native int get_number_of_modules( int slot );
+	public static native int get_module_flags( int slot, int mod_num );
+	public static native int[] get_module_inputs( int slot, int mod_num );
+	public static native int[] get_module_outputs( int slot, int mod_num );
+	public static native string get_module_name( int slot, int mod_num );
+	public static native int get_module_xy( int slot, int mod_num );
+	public static native int get_module_color( int slot, int mod_num );
+	public static native int get_module_scope( int slot, int mod_num, int channel, short[] read_buf, int samples_to_read );
+	public static native int get_number_of_module_ctls( int slot, int mod_num );
+	public static native string get_module_ctl_name( int slot, int mod_num, int ctl_num );
+	public static native int get_module_ctl_value( int slot, int mod_num, int ctl_num, int scaled );
+	public static native int get_number_of_patterns( int slot );
+	public static native int get_pattern_x( int slot, int pat_num );
+	public static native int get_pattern_y( int slot, int pat_num );
+	public static native int get_pattern_tracks( int slot, int pat_num );
+	public static native int get_pattern_lines( int slot, int pat_num );
+	public static native byte[] get_pattern_data( int slot, int pat_num );
+	public static native int set_pattern_data( int slot, int pat_num, byte[] pat_data );
+	public static native int pattern_mute( int slot, int pat_num, int mute );
+	public static native int get_ticks();
+	public static native int get_ticks_per_second();
+}
+
---
+++ readme.txt
@@ -0,0 +1,31 @@
+[ About ]
+
+With this library you can play SunVox songs and synths in your applications.
+SunVox is a small, fast and powerful modular synthesizer with pattern-based sequencer (tracker):
+http://www.warmplace.ru/soft/sunvox
+
+[ Structure ]
+
+headers - SunVox C/C++/Pixilang header files.
+ios - SunVox library for iOS + sample projects.
+linux - SunVox library for Linux + sample projects.
+linux_android - SunVox library for Android + sample projects.
+osx - SunVox library for OSX + sample projects.
+pixilang - example of using SunVox library in Pixilang application.
+resources - SunVox songs and modules for the sample projects.
+windows - SunVox library for Windows + sample projects.
+
+[ Donation ]
+
+Please make a donation if you use this library in your projects:
+http://www.warmplace.ru/donate
+
+[ License ]
+
+You can use SunVox library freely, but the following text should be included in your products (e.g. in About window).
+
+SunVox modular synthesizer
+Copyright (c) 2008 - 2016, Alexander Zolotov <nightradio@gmail.com>, WarmPlace.ru
+
+Ogg Vorbis 'Tremor' integer playback codec
+Copyright (c) 2002, Xiph.org Foundation
